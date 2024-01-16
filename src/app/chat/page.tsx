86d6608254d1b9f11e26324ae03b577531620192
Jan Milenkov <jan.milenkov@globalinfra.tech>
2024-01-16 23:31:46 +0200

    added chat page with approporate scroll formatting

--- src/app/chat/page.tsx
+++ src/app/chat/page.tsx
@@ -17,15 +17,16 @@ export default function Chat() {
       {/* Searchbar */}
       <div className="w-full px-6 py-4">
         <div
-          className={`flex w-full flex-row justify-around rounded-xl border-t border-t-gray-300 bg-blue-50 drop-shadow-lg transition-all ${
-            flash
-              ? 'animate-flash border-1 border-gray-100 shadow-2xl shadow-cyan-300'
-              : ''
-          }`}
+          className={`flex w-full flex-row justify-around rounded-xl border-t border-t-gray-300 bg-blue-50 
+        drop-shadow-lg transition-all ${
+          flash
+            ? 'animate-flash border-1 border-gray-100 shadow-2xl shadow-cyan-300'
+            : ''
+        }`}
         >
           <input
             className="m-1 h-full w-full rounded-l-3xl bg-transparent p-2
-														focus:bg-gray-50 focus:bg-transparent focus:outline-0"
+								 focus:bg-gray-50 focus:bg-transparent focus:outline-0"
           />
           <button
             className={`rounded-r-3xl pr-3 transition-transform ${
@@ -34,13 +35,12 @@ export default function Chat() {
             onClick={handleClick}
           >
             <svg
-              xmlns="http://www.w3.org/2000/svg"
               width="28"
               height="28"
               viewBox="0 0 50 50"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <path
-                d="M 21 3 C 11.622998 3 4 10.623005 4 20 C 4 29.376995 11.622998 37 21 37 C 24.712383 37 28.139151 35.791079 30.9375 33.765625 L 44.085938 46.914062 L 46.914062 44.085938 L 33.886719 31.058594 C 36.443536 28.083 38 24.223631 38 20 C 38 10.623005 30.377002 3 21 3 z M 21 5 C 29.296122 5 36 11.703883 36 20 C 36 28.296117 29.296122 35 21 35 C 12.703878 35 6 28.296117 6 20 C 6 11.703883 12.703878 5 21 5 z"></path>
+              <path d="M 21 3 C 11.622998 3 4 10.623005 4 20 C 4 29.376995 11.622998 37 21 37 C 24.712383 37 28.139151 35.791079 30.9375 33.765625 L 44.085938 46.914062 L 46.914062 44.085938 L 33.886719 31.058594 C 36.443536 28.083 38 24.223631 38 20 C 38 10.623005 30.377002 3 21 3 z M 21 5 C 29.296122 5 36 11.703883 36 20 C 36 28.296117 29.296122 35 21 35 C 12.703878 35 6 28.296117 6 20 C 6 11.703883 12.703878 5 21 5 z"></path>
             </svg>
           </button>
         </div>
@@ -52,19 +52,19 @@ export default function Chat() {
         <div className="mr-4 h-12 w-12 flex-none last:mr-0">
           <div className="flex h-12 w-12 items-center justify-center rounded-full bg-blue-500 align-middle">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
+              className="feather feather-plus"
               viewBox="0 0 24 24"
               fill="none"
+              width="30"
+              height="30"
               stroke="white"
               strokeWidth="1.8"
               strokeLinecap="round"
               strokeLinejoin="round"
-              className="feather feather-plus"
-              width="30"
-              height="30"
+              xmlns="http://www.w3.org/2000/svg"
             >
-              <line x1="12" y1="5" x2="12" y2="19"></line>
-              <line x1="5" y1="12" x2="19" y2="12"></line>
+              <line x1="12" y1="5" x2="12" y2="19" />
+              <line x1="5" y1="12" x2="19" y2="12" />
             </svg>
           </div>
           <p className="w-12 truncate pt-2 text-center text-xs font-medium capitalize text-gray-600">
@@ -80,11 +80,11 @@ export default function Chat() {
           'Anaastasiq',
           'Anatahuan',
           'Anatahuan',
-          'Anatahuan'
+          'Anatahuan',
         ].map((item, index) => (
           <div
-            className="mr-4 flex w-14 flex-none flex-col items-center justify-center last:mr-0"
             key={index}
+            className="mr-4 flex w-14 flex-none flex-col items-center justify-center last:mr-0"
           >
             <div className="h-12 w-12 rounded-full border-2 border-white bg-green-400 outline outline-blue-500" />
             <p className="w-16 truncate pt-2 text-center text-xs font-medium capitalize text-gray-600">
@@ -102,96 +102,95 @@ export default function Chat() {
             fullName: 'Smith Richardson',
             lastMessage: 'We should meet up soon',
             time: '5:30 PM',
-            missedCount: 0
+            missedCount: 0,
           },
           {
             fullName: 'John Doe',
             lastMessage: 'How are you doing?',
             time: '3:45 PM',
-            missedCount: 3
+            missedCount: 3,
           },
           {
             fullName: 'Alice Johnson',
             lastMessage: 'Great news!',
             time: '1:20 PM',
-            missedCount: 10
+            missedCount: 10,
           },
           {
             fullName: 'Bob Smith',
             lastMessage: 'Where are you?',
             time: '10:15 AM',
-            missedCount: 3
+            missedCount: 3,
           },
           {
             fullName: 'Emily White',
             lastMessage: 'Happy birthday!',
             time: '8:00 AM',
-            missedCount: 1
+            missedCount: 1,
           },
           {
             fullName: 'David Brown',
-            lastMessage: 'Let\'s grab lunch',
+            lastMessage: "Let's grab lunch",
             time: '6:30 AM',
-            missedCount: 2
+            missedCount: 2,
           },
           {
             fullName: 'Sophia Taylor',
             lastMessage: 'Thank you!',
             time: 'Yesterday',
-            missedCount: 0
+            missedCount: 0,
           },
           {
             fullName: 'Michael Wilson',
             lastMessage: 'Check this out',
             time: '2 days ago',
-            missedCount: 0
+            missedCount: 0,
           },
           {
             fullName: 'Olivia Martin',
-            lastMessage: 'I\'m on my way',
+            lastMessage: "I'm on my way",
             time: '3 days ago',
-            missedCount: 4
+            missedCount: 4,
           },
           {
             fullName: 'James Anderson',
-            lastMessage: 'Don\'t forget',
+            lastMessage: "Don't forget",
             time: '4 days ago',
-            missedCount: 0
+            missedCount: 0,
           },
           {
             fullName: 'Emma Harris',
             lastMessage: 'See you later',
             time: '5 days ago',
-            missedCount: 0
+            missedCount: 0,
           },
           {
             fullName: 'William Clark',
-            lastMessage: 'It\'s urgent',
+            lastMessage: "It's urgent",
             time: '1 week ago',
-            missedCount: 0
+            missedCount: 0,
           },
           {
             fullName: 'Ava Lewis',
             lastMessage: 'Missed your call',
             time: '2 weeks ago',
-            missedCount: 1
+            missedCount: 1,
           },
           {
             fullName: 'Ethan Turner',
             lastMessage: 'Happy holidays!',
             time: '3 weeks ago',
-            missedCount: 4
+            missedCount: 4,
           },
           {
             fullName: 'Mia Adams',
-            lastMessage: 'Let\'s plan something',
+            lastMessage: "Let's plan something",
             time: '4 weeks ago',
-            missedCount: 0
-          }
+            missedCount: 0,
+          },
         ].map((item, index) => (
           <div className="flex w-full px-5 py-3" key={index}>
-            <div
-              className="aspect-square h-16 rounded-full border-2 border-white bg-blue-500 outline outline-blue-500" />
+            <div className="aspect-square h-16 rounded-full border-2 border-white bg-blue-500 outline outline-blue-500" />
             <div className="ml-4 h-full w-full pt-2">
               <label className="font-mono text-lg capitalize">
                 {item.fullName}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
+      <html lang="en" className="h-screen">
         <body className={inter.className}>{children}</body>
       </html>
     </ClerkProvider>
